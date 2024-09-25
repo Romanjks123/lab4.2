494bb5f4c3b000734fbff67f92d59a8785aaefc2
Romanjks123 <m7playyt@gmail.com>
2024-09-26 00:52:12 +0300

    first commit

---
+++ 4.2.cpp
@@ -0,0 +1,41 @@
+// Lab_03_1.cpp
+// < Огоновський Роман >
+// Лабораторна робота № 4.2
+// Табуляція функції, заданої
+// Табуляція функції, заданої формулою: функція однієї змінної.
+// Варіант 24
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x, xp, xk, dx, A, B, y;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << fixed;
+	cout << "---------------------------" << endl;
+	cout << "|" << setw(5) << "x" << " |"
+		<< setw(7) << "y" << " |" << endl;
+	cout << "---------------------------" << endl;
+	x = xp;
+	while (x <= xk)
+	{
+		A = x;
+		if (x <= -1)
+			B = log(fabs(cos(5*x)))+exp((1/x)+x);
+		else
+			if (x >= 0.4)
+				B = sin(5*x)-sqrt(fabs(1-x));
+			else
+				B = sqrt(pow(2-x, 3))-tan(x);
+		y = A + B;
+		cout << "|" << setw(7) << setprecision(2) << x
+			<< " |" << setw(10) << setprecision(3) << y
+			<< " |" << endl;
+		x += dx;
+	}
+	cout << "---------------------------" << endl;
+	return 0;}
